34b074eb306cfac95297c059a0df34d870473447
yuta.iwasaki <yuta825217@gmail.com>
2019-06-02 05:23:57 +0000

    post

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,5 @@ Rails.application.routes.draw do
    
    get 'signup', to: 'users#new'
   resources :users, only: [:index, :show, :new, :create]
+  resources :microposts, only: [:create, :destroy]
 end
